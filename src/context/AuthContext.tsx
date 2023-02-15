388d06f39abc8311b80f4dd3e3246fd1126661c4
Remees Salam <remeessalam@Remeess-Air.bbrouter>
2023-02-15 12:44:46 +0530

    that problem solved and login and signup is completed chat is near to completion

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,82 +1,203 @@
-import { useMutation } from "@tanstack/react-query";
-import { UseMutationResult } from "@tanstack/react-query/build/lib/types";
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
-import axios, { AxiosResponse } from 'axios';
-import { useNavigate } from "react-router";
-import { StreamChat } from "stream-chat";
+// import { useMutation } from "@tanstack/react-query";
+// import { UseMutationResult ,UseMutationOptions} from "@tanstack/react-query/build/lib/types";
+// import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+// import axios, { AxiosResponse } from 'axios';
+// import { useNavigate } from "react-router";
+// import { StreamChat } from "stream-chat";
+
+// type AuthContext = {
+//     user?: User
+//     streamChat?: StreamChat
+//     signup: UseMutationResult<AxiosResponse, unknown, User>
+//     login: UseMutationResult<{ token: string, user: User }, unknown, string>
+// }
+// type User = {
+//     id: string
+//     name: string
+//     image?: string
+// }
+
+// const Context = createContext<AuthContext | null>(null);
+
+// export function useAuth() {
+//     return useContext(Context) as AuthContext;
+// }
+
+// type AuthProviderProps = {
+//     children: ReactNode
+// }
+
+// export function AuthProvider({ children }: AuthProviderProps) {
+//     const navigate = useNavigate()
+//     const [user, setUser] = useState<User>()
+//     const [token, setToken] = useState<string>()
+//     const [streamChat, setStreamChat] = useState<string>()
+//     const signup = useMutation({
+//         mutationFn: (user: User) => {
+//             return axios.post(`${import.meta.env.VITE_SERVER_URL}/signup`, user)
+//         },
+//         onSuccess() {
+//             navigate("/login")
+//         }
+//     })
+//     const login = useMutation({
+//         mutationFn: (id: string) => {
+//             return axios
+//                 .post(`${import.meta.env.VITE_SERVER_URL}/login`, { id })
+//                 .then(res => {
+//                     return res.data as { token: string; user: User }
+//                 })
+//         },
+//         onSuccess(data) {
+//             setUser(data.user)
+//             setToken(data.token)
+//         }
+//     })
+//     useEffect(() => {
+//         if (token == null || user == null) return
+//         const chat = new StreamChat(import.meta.env.VITE_STREAM_API_KEY!)
+
+//         if (chat.tokenManager.token === token && chat.userID === user.id) return
+
+//         let isInterrupted = false
+
+//         const connectPromise = chat.connectUser(user, token).then(() => {
+//             if (isInterrupted) return
+//             setStreamChat(chat)
+//         })
+//         return () => {
+//             isInterrupted = true
+//             setStreamChat(undefined)
+
+//             connectPromise.then(() => {
+//                 chat.disconnectUser()
+//             })
+//         }
+//     }, [token, user])
+//     return (
+//         <Context.Provider value={{ signup, login, user, streamChat }}>
+//             {children}
+//         </Context.Provider>
+//     )
+// }
+
+
+
+
+
+
+
+import { useMutation } from "@tanstack/react-query"
+import {
+  UseMutationOptions,
+  UseMutationResult,
+} from "@tanstack/react-query/build/lib/types"
+import axios, { AxiosResponse } from "axios"
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react"
+import { useNavigate } from "react-router-dom"
+import { StreamChat } from "stream-chat"
+import { useLocalStorage } from "../hooks/useLocalStorage"
 
 type AuthContext = {
-    user?: User
-    streamChat?: StreamChat
-    signup: UseMutationResult<AxiosResponse, unknown, User>
-    login: UseMutationResult<{ token: string, user: User }, unknown, string>
-}
-type User = {
-    id: string
-    name: string
-    image?: string
+  user?: User
+  streamChat?: StreamChat
+  signup: UseMutationResult<AxiosResponse, unknown, User>
+  login: UseMutationResult<{ token: string; user: User }, unknown, string>
+  logout: UseMutationResult<AxiosResponse, unknown, void>
 }
 
-const Context = createContext<AuthContext | null>(null);
+type User = {
+  id: string
+  name: string
+  image?: string
+}
+
+const Context = createContext<AuthContext | null>(null)
 
 export function useAuth() {
-    return useContext(Context) as AuthContext;
+  return useContext(Context) as AuthContext
+}
+
+export function useLoggedInAuth() {
+  return useContext(Context) as AuthContext &
+    Required<Pick<AuthContext, "user">>
 }
 
 type AuthProviderProps = {
-    children: ReactNode
+  children: ReactNode
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
-    const navigate = useNavigate()
-    const [user, setUser] = useState<User>()
-    const [token, setToken] = useState<string>()
-    const [streamChat, setStreamChat] = useState<string>()
-    const signup = useMutation({
-        mutationFn: (user: User) => {
-            return axios.post(`${import.meta.env.VITE_SERVER_URL}/signup`, user)
-        },
-        onSuccess() {
-            navigate("/login")
-        }
-    })
-    const login = useMutation({
-        mutationFn: (id: string) => {
-            return axios
-                .post(`${import.meta.env.VITE_SERVER_URL}/login`, { id })
-                .then(res => {
-                    return res.data as { token: string; user: User }
-                })
-        },
-        onSuccess(data) {
-            setUser(data.user)
-            setToken(data.token)
-        }
-    })
-    useEffect(() => {
-        if (token == null || user == null) return
-        const chat = new StreamChat(import.meta.env.VITE_STREAM_API_KEY!)
+  const navigate = useNavigate()
+  const [user, setUser] = useLocalStorage<User>("user")
+  const [token, setToken] = useLocalStorage<string>("token")
+  const [streamChat, setStreamChat] = useState<StreamChat>()
 
-        if (chat.tokenManager.token === token && chat.userID === user.id) return
+  const signup = useMutation({
+    mutationFn: (user: User) => {
+      return axios.post(`${import.meta.env.VITE_SERVER_URL}/signup`, user)
+    },
+    onSuccess() {
+      navigate("/login")
+    },
+  })
 
-        let isInterrupted = false
-
-        const connectPromise = chat.connectUser(user, token).then(() => {
-            if (isInterrupted) return
-            setStreamChat(chat)
+  const login = useMutation({
+    mutationFn: (id: string) => {
+      return axios
+        .post(`${import.meta.env.VITE_SERVER_URL}/login`, { id })
+        .then(res => {
+          return res.data as { token: string; user: User }
         })
-        return () => {
-            isInterrupted = true   
-            setStreamChat(undefined)
+    },
+    onSuccess(data) {
+      setUser(data.user)
+      setToken(data.token)
+    },
+  })
 
-            connectPromise.then(() => {
-                chat.disconnectUser()
-            })
-        }
-    }, [token, user])
-    return (
-        <Context.Provider value={{ signup, login, user , streamChat}}>
-            {children}
-        </Context.Provider>
-    )
+  const logout = useMutation({
+    mutationFn: () => {
+      return axios.post(`${import.meta.env.VITE_SERVER_URL}/logout`, { token })
+    },
+    onSuccess() {
+      setUser(undefined)
+      setToken(undefined)
+      setStreamChat(undefined)
+    },
+  })
+
+  useEffect(() => {
+    if (token == null || user == null) return
+    const chat = new StreamChat(import.meta.env.VITE_STREAM_API_KEY!)
+
+    if (chat.tokenManager.token === token && chat.userID === user.id) return
+
+    let isInterrupted = false
+    const connectPromise = chat.connectUser(user, token).then(() => {
+      if (isInterrupted) return
+      setStreamChat(chat)
+    })
+
+    return () => {
+      isInterrupted = true
+      setStreamChat(undefined)
+
+      connectPromise.then(() => {
+        chat.disconnectUser()
+      })
+    }
+  }, [token, user])
+
+  return (
+    <Context.Provider value={{ signup, login, user, streamChat, logout }}>
+      {children}
+    </Context.Provider>
+  )
 }
--- src/pages/layouts/RootLayout.tsx
+++ src/pages/layouts/RootLayout.tsx
@@ -1,8 +1,11 @@
-import { Outlet } from "react-router-dom";
-import { FullScreenCard } from "../../components/FullScreenCard";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+
+
 
 export function RootLayout() {
-    return (
-        <Outlet />
-    )
+    const { user } = useAuth();
+    if (user == null) return <Navigate to="/login" />
+
+    return <Outlet />
 }
